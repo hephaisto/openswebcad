c68f2abaf0d9d99e5faec3ffc17673c7d23aec35
hephaisto <1156478+hephaisto@users.noreply.github.com>
2025-01-15 10:42:38 +0100

    move python files

--- src/openswebcad/src/openswebcad/__init__.py
+++ src/openswebcad/src/openswebcad/__init__.py
@@ -1,3 +1,29 @@
-# SPDX-FileCopyrightText: 2025-present U.N. Owen <void@some.where>
-#
-# SPDX-License-Identifier: MIT
+from abc import ABC
+from pydantic import BaseModel
+
+from openswebcad.annotations import *
+
+class GenerationError(RuntimeError):
+    pass
+
+class OpenScadScriptError(GenerationError):
+    def __init__(self, script: str, stderr: str):
+        self.script = script
+        self.stderr = stderr
+
+    def __str__(self):
+        return "model generation failed"
+
+class IncompatibleParametersError(GenerationError):
+    def __init__(self, parameters: list[str], message: str):
+        self.parameters = parameters
+        self.message = message
+
+    def __str__(self):
+        separator = ": " if self.message else ""
+        return "The following parameters have incompatible values: " + ', '.join(self.parameters) + separator + self.message
+
+class ModelError(GenerationError):
+    def __str__(self):
+        return "model generation failed"
+
